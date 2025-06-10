ee4fda45f15f023decb3a3d150d2fbfd99fb45da
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-11 11:26:24 +1200

    Revert "Reapply "Merge branch 'master' into will""
    This reverts commit 115560ebfa6c56288fcc73915f96d31232366be3.

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const max_speed = 1800.0
+const max_speed = 2000
 var velocity_vector = Vector2(0,-1)
 var speed = 0 #px/s
 var acceleration = 300 #px/s^2
@@ -15,12 +15,6 @@ var prev_speed = 0
 var start_pos
 var roating = false
 var fix_spin = false
-var fuel = 150
-var fuel_max = 150
-var fuel_loss_rate = 10 # KiloGallons per second
-var boost_cooldown = true
-
-var slide = false
 
 func _ready():
 	start_pos = position
@@ -28,15 +22,10 @@ func _ready():
 func angle_to_vector(): # read the function name fuckwad
 	velocity_vector = Vector2(sin(angle), -cos(angle))
 
-func _process(delta):
-	if fuel <= 0:
-		pass
-	$CanvasLayer/FuelTank/ColorRect.get_material().set_shader_parameter("level", 1-(fuel/fuel_max))
-	fuel -= fuel_loss_rate * delta
-	
 
 func _physics_process(delta):
-	if Input.is_action_pressed("accelerate") and not slide:
+	if Input.is_action_pressed("accelerate"):
+		#print(speed-prev_speed)
 		prev_speed = speed # for testing
 		
 		# acceleration is scaled so that its slower at higher speeds
@@ -72,22 +61,14 @@ func _physics_process(delta):
 		if angle > 2*PI:
 			angle += 2*PI
 		angle_to_vector()
-	
-	if Input.is_action_pressed("boost")and boost_cooldown:
-		speed += 1000
-		fuel -= 20
-		boost_cooldown = false
-		$boost_timer.start()
 		
 	#flag checks if you are turning to reset turn_rate - see above code
 	if flag == false:
 		turn_rate = 0
 	if Input.is_action_pressed("brake"):
 		speed -= brake_rate * delta
-	if Input.is_action_pressed("drift"):
-		turn_rate += turn_rate_rate*delta*2
 	
-	if roating and not slide:
+	if roating:
 		angle += delta*PI*10*randf_range(1.9,2.1)
 	
 	rotation = angle
@@ -95,22 +76,16 @@ func _physics_process(delta):
 	velocity = velocity_vector * speed
 	move_and_slide()
 	
-func _on_slide_1_body_entered(body: Node2D) -> void:
-	if not body.is_in_group('player'):
-		slide = true
-
-func _on_slide_2_body_entered(body: Node2D) -> void:
-	if not body.is_in_group('player'):
-		slide = true
+	
 
 
 func _on_area_2d_body_entered(body):
 	if not body.is_in_group('player'):
-		if speed >= 1300 and not slide:
+		if speed >= 1300:
 			$spin_timer.start(0.5)
 			roating = true
-	if not slide and speed >= 300:
 		speed = -speed/2
+		
 
 func _on_spin_timer_timeout() -> void:
 	roating = false
@@ -120,19 +95,3 @@ func _on_spin_timer_timeout() -> void:
 
 func _on_fix_spin_timeout() -> void:
 	fix_spin = false
-	slide = false
-	friction = 500
-
-
-func _on_boost_timer_timeout() -> void:
-	boost_cooldown = true
-	
-
-
-func _on_slide_1_body_exited(body: Node2D) -> void:
-	if not body.is_in_group('player'):
-		slide = false
-
-func _on_slide_2_body_exited(body: Node2D) -> void:
-	if not body.is_in_group('player'):
-		slide = false
--- scripts/minimap.gd
+++
@@ -1,8 +0,0 @@
-extends SubViewport
-
-func _ready():
-	world_2d = get_tree().root.world_2d
-
-func _process(delta: float) -> void:
-	$MinimapCamera.position = get_parent().get_parent().get_parent().position
-	
