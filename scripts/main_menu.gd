7c3cc8afe416e08371c8891e5f7eb898beead057
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-18 14:35:11 +1200

    Level Instancing

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,17 +4,6 @@ var levelSelectButton = preload("res://scenes/level_select_button.tscn")
 
 var levels = {
 	1: "scenes/mapOne.tscn",
-	2: "scenes/mapOne.tscn",
-	3: "scenes/mapOne.tscn",
-	4: "scenes/mapOne.tscn",
-	5: "scenes/mapOne.tscn",
-	6: "scenes/mapOne.tscn",
-	7: "scenes/mapOne.tscn",
-	8: "scenes/mapOne.tscn",
-	9: "scenes/mapOne.tscn",
-	10: "scenes/mapOne.tscn",
-	11: "scenes/mapOne.tscn",
-	12: "scenes/mapOne.tscn"
 }
 
 func _on_play_button_pressed() -> void:
